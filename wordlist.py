98612d7503b776cb11d663751a7543ae97b53d13
Gaurav Taxali <61114467+gaurav16-byte@users.noreply.github.com>
2020-12-30 14:32:03 +0530

    Update wordlist.py

--- wordlist.py
+++ wordlist.py
@@ -1,18 +1,19 @@
 # [len]--1 chars--2 name_of_file--3
-from itertools import product
+from itertools import product 
 import sys
 import os
+
 filename = os.getenv('HOME') + '/' + sys.argv[3]
 b = []
 
 while int(sys.argv[1])<1:
 	print('Passwords of 0 length cannot be formed')
 else:
-	file = open(filename,'w')
+	file = open(filename,'w')						
 	words = sys.argv[2].split(sep = ',')
 	password = ''
 	for word in words:
-		p = product(word,repeat = int(sys.argv[1]))
+		p = product(word,repeat = int(sys.argv[1]))		#permutations
 		for i in p:
 			b = list(i)
 			for j in b:
@@ -21,4 +22,6 @@ else:
 			password = ''
 
 file.close()
-print('The file ' + sys.argv[3] + ' has been created')
+g = open(filename,'r')
+length = len(g.readlines())
+print('The file ' + sys.argv[3] + ' has been created with ',length,' passwords')
